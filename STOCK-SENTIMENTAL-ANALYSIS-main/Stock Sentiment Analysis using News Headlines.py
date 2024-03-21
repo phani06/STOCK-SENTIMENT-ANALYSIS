28825728792203196bc1c64db1c612cd6a29e9c4
Phanindhar T <67418559+phani06@users.noreply.github.com>
2024-03-20 23:34:22 -0400

    Update Stock Sentiment Analysis using News Headlines.py

--- STOCK-SENTIMENTAL-ANALYSIS-main/Stock Sentiment Analysis using News Headlines.py
+++ STOCK-SENTIMENTAL-ANALYSIS-main/Stock Sentiment Analysis using News Headlines.py
@@ -1,121 +1,82 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
-####Stock Sentiment Analysis using News Headlines
-
-
-# In[1]:
-
-
+# Importing necessary libraries
 import pandas as pd
-
-
-# In[2]:
-
-
-df=pd.read_csv('Data.csv', encoding = "ISO-8859-1")
-
-
-# In[3]:
-
-
-df.head()
-
-
-# In[4]:
-
-
-train = df[df['Date'] < '20150101']
-test = df[df['Date'] > '20141231']
-
-
-# In[5]:
-
-
-# Removing punctuations
-data=train.iloc[:,2:27]
-data.replace("[^a-zA-Z]"," ",regex=True, inplace=True)
-
-# Renaming column names for ease of access
-list1= [i for i in range(25)]
-new_Index=[str(i) for i in list1]
-data.columns= new_Index
-data.head(5)
-
-
-# In[6]:
-
-
-for index in new_Index:
-   data[index]=data[index].str.lower()
-data.head(1)
-
-
-# In[7]:
-
-
-' '.join(str(x) for x in data.iloc[1,0:25])
-
-
-# In[8]:
-
-
-headlines = []
-for row in range(0,len(data.index)):
-    headlines.append(' '.join(str(x) for x in data.iloc[row,0:25]))
-
-
-# In[9]:
-
-
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
+# Function to load and preprocess the data
+def load_data(file_path):
+    # Read CSV file
+    df = pd.read_csv(file_path, encoding="ISO-8859-1")
 
-# In[10]:
+    # Splitting into training and testing sets
+    train = df[df['Date'] < '20150101']
+    test = df[df['Date'] > '20141231']
 
+    # Preprocessing headlines
+    train_headlines = preprocess_headlines(train)
+    test_headlines = preprocess_headlines(test)
 
-#implement BAG OF WORDS
-countvector=CountVectorizer(ngram_range=(2,2))
-traindataset=countvector.fit_transform(headlines)
+    return train_headlines, test_headlines, train['Label'], test['Label']
 
+# Function to preprocess headlines
+def preprocess_headlines(df):
+    # Combine headlines into one string
+    headlines = df.apply(lambda row: ' '.join(str(x).lower() for x in row[2:27]), axis=1)
+    return headlines
 
-# In[11]:
+# Function to train the model
+def train_model(train_headlines, labels):
+    # Vectorize the headlines
+    vectorizer = CountVectorizer(ngram_range=(2, 2))
+    train_dataset = vectorizer.fit_transform(train_headlines)
 
+    # Train RandomForest Classifier
+    random_forest_classifier = RandomForestClassifier(n_estimators=200, criterion='entropy')
+    random_forest_classifier.fit(train_dataset, labels)
 
-# implement RandomForest Classifier
-randomclassifier=RandomForestClassifier(n_estimators=200,criterion='entropy')
-randomclassifier.fit(traindataset,train['Label'])
+    return random_forest_classifier, vectorizer
 
+# Function to test the model
+def test_model(model, vectorizer, test_headlines, labels):
+    # Transform test dataset
+    test_dataset = vectorizer.transform(test_headlines)
 
-# In[12]:
+    # Make predictions
+    predictions = model.predict(test_dataset)
 
+    # Evaluate the model
+    evaluate_model(labels, predictions)
 
-# Predict for the Test Dataset
-test_transform= []
-for row in range(0,len(test.index)):
-    test_transform.append(' '.join(str(x) for x in test.iloc[row,2:27]))
-test_dataset = countvector.transform(test_transform)
-predictions = randomclassifier.predict(test_dataset)
+# Function to evaluate the model
+def evaluate_model(true_labels, predicted_labels):
+    # Confusion matrix
+    matrix = confusion_matrix(true_labels, predicted_labels)
+    print("Confusion Matrix:")
+    print(matrix)
 
+    # Accuracy score
+    accuracy = accuracy_score(true_labels, predicted_labels)
+    print("\nAccuracy Score:", accuracy)
 
-# In[13]:
+    # Classification report
+    report = classification_report(true_labels, predicted_labels)
+    print("\nClassification Report:")
+    print(report)
 
+# Main function
+def main():
+    # Load and preprocess data
+    train_headlines, test_headlines, train_labels, test_labels = load_data('Data.csv')
 
-## Import library to check accuracy
-from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
+    # Train the model
+    model, vectorizer = train_model(train_headlines, train_labels)
 
+    # Test the model
+    test_model(model, vectorizer, test_headlines, test_labels)
 
-# In[14]:
-
-
-matrix=confusion_matrix(test['Label'],predictions)
-print(matrix)
-score=accuracy_score(test['Label'],predictions)
-print(score)
-report=classification_report(test['Label'],predictions)
-print(report)
-
+if __name__ == "__main__":
+    main()
